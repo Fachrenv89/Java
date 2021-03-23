cb380855efca614610f0158da3880095cf7a8685
Farenn <80033281+Fachrenv89@users.noreply.github.com>
2021-03-23 18:10:01 +0700

    Add files via upload

---
+++ app/java1/AppJava1.java
@@ -0,0 +1,15 @@
+package app.java1;
+
+public class AppJava1 {
+    public static void main(String[] args) {
+        // jika ingin menghitung int panjang 
+        int panjang = 100;
+        // jika ingin menghitung int lebar
+        int lebar   = 809;
+        // int luas ini gabungan dari kedua int panjang dan int lebar
+        int luas;
+        luas = panjang * lebar;
+        // keluarkan hasil output seperti ini 
+        System.out.println(luas);
+    }  
+}
